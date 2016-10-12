72af8717aefceea26eace592deb13506adf39066
David Pett <dpett@folklore.digital>
2016-10-12 13:49:30 -0500

    DIE fix

--- addon/components/bodymovin.js
+++ addon/components/bodymovin.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['bodymovin'],
 
+  bodymovin: null,
   path: null,
   loop: true,
   autoplay: true,
@@ -11,22 +12,27 @@ export default Ember.Component.extend({
   renderType: "svg",
 
   setupBodymovin: Ember.on('didInsertElement', function() {
-    let animation = bodymovin.loadAnimation({
-      container: document.getElementById(this.get('elementId')),
-      autoloadSegments: this.get('autoloadSegments'),
-      renderer: this.get('renderType'),
-      prerender: this.get('prerender'),
-      autoplay: this.get('autoplay'),
-      loop: this.get('loop'),
-      path: (!this.get('external')) ? `animations/${this.get('path')}.json` : this.get('path') 
-    });
-
-    this.sendAction('onReady', animation);
-
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      let animation = bodymovin.loadAnimation({
+        container: document.getElementById(this.get('elementId')),
+        autoloadSegments: this.get('autoloadSegments'),
+        renderer: this.get('renderType'),
+        prerender: this.get('prerender'),
+        autoplay: this.get('autoplay'),
+        loop: this.get('loop'),
+        path: (!this.get('external')) ? `animations/${this.get('path')}.json` : this.get('path')
+      });
+      this.set('bodymovin', animation);
+      this.sendAction('onReady', animation);
+    })
   }),
 
   teardownBodymovin: Ember.on('willDestroyElement', function() {
-    this.get('bodymovin').destroy();
+    let bodymovin = this.get('bodymovin');
+    if (bodymovin) {
+      bodymovin.destroy();
+      this.set('bodymovin', null);
+    }
   })
 
 });
